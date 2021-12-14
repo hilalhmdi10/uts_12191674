76db8e9227a9416eec3651ee41292cae238954c8
hilalhamdi <hilalhmdi10@gmail.com>
2021-12-14 20:43:15 +0700

    first commit

---
+++ pointkedua.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Hilal Hamdi Hafit";
+$nim = 12191674;
+$umur = 19;
+echo "Nama Saya : $nama ";
+echo "<br />";
+echo "Nim  Saya : $nim ";
+echo "<br />";
+echo "Umur Saya : $umur Tahun ";
+echo "<br />";
+echo date('d-m-Y',strtotime('2017-03-14'));
---
+++ pointpertama.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Hilal Hamdi Hafit";
+$nim = 12191674;
+$umur = 19;
+$ikanku = array ("Teri","Lele","Bawal","Tuna");
+echo "<li>Nama Saya : $nama </li>";
+echo "<li>Nim  Saya : $nim </li>";
+echo "<li>Umur Saya : $umur Tahun </li>";
+echo "<li>Ikan $ikanku[0] sering dijumpai di nasi kucing 
+angkringan dan saya konsumsi pas tanggal tua. 
+Kalo tanggal muda sering kuliner $ikanku[1], $ikanku[2],
+dan $ikanku[3]</li>";
+?>
